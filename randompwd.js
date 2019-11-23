eb999e4f99a45ed81834673265c17ee0d52f087f
raquelacevedo <raquel.e.acevedo@gmail.com>
2019-11-23 17:46:14 -0500

    trying to get buttons to work

--- randompwd.js
+++ randompwd.js
@@ -1,14 +1,15 @@
 function generate( len ) {
-    var length = 10;
-
+//    var length = "";
+    //set password length/complexity
+       var pwdlen = document.getElementById("userlength").value;
        //string of values that make a strong password
        var values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
 
        var password = "";
    
-       //create for loop to choose password characters
-       for(var i = 0; i <= length; i++){
-           password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length)));
+       //create for loop to choose password characters based on the length received from user
+       for(var i = 0; i <= pwdlen; i++){
+           password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
        } 
 
         //add password to textbox/display area
@@ -16,7 +17,20 @@ function generate( len ) {
 
 }
 
+//set default length display of 10
+document.getElementById("length").innerHTML = "Length: 10";
 
+//Use length to return password string
+document.getElementById("userlength").oninput = function(){
+
+    if(document.getElementById("userlength").value > 0){
+        document.getElementById("length").innerHTML = "length: " + document.getElementById("userlength").value;
+    }
+    else{
+        document.getElementById("length").innerHTML = "Length: 1";
+    }
+
+}
 
 var output = document.getElementById('output');
 
@@ -33,3 +47,14 @@ function copyPassword(){
     alert("Your Strong Password has been copied to the clipboard!");
 
 }
+
+//buttons...not working as designed :-(
+function manage(display) {
+    var bt = document.getElementById('num');
+    if (display.value != '') {
+        bt.disabled = false;
+    }
+    else {
+        bt.disabled = true;
+    }
+}  
